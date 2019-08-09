0f65b8dd41af8eedfb48b48f2a5af2a1d0832393
Chris Olsen <olsen.chris@gmail.com>
2019-08-09 17:15:56 -0600

    lint fixes

--- longest_substring_wo_repeating_chars.py
+++ longest_substring_wo_repeating_chars.py
@@ -1,5 +1,6 @@
 # Hi, here's your problem today. This problem was recently asked by Microsoft:
-# Given a string, find the length of the longest substring without repeating characters.
+# Given a string, find the length of the longest substring without repeating
+# characters.
 
 # class Solution:
 #   def lengthOfLongestSubstring(self, s):
@@ -10,16 +11,18 @@
 
 # Can you find a solution in linear time?
 
+
 def longest(input):
     chars, count, max_count = {}, 0, 0
     for c in input:
-        if chars.has_key(c):
+        if c in chars:
             max_count = count if count > max_count else max_count
             chars, count = {}, 0
         count += 1
         chars[c] = True
 
     return max_count
+
 
 if __name__ == '__main__':
     count = longest('abrkaabcdefghijjxxx')
